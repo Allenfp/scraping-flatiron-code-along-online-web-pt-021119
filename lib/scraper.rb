c6e7da33ff9e96b6bb8c0cd3a3f9bfb2aaf97834
Allen F Partlow <allenfp@gmail.com>
2019-03-28 14:42:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,9 +21,9 @@ class Scraper
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
       # binding.pry
     end
-  
-    def get_courses
       
+    def get_courses
+      self.get_page.css(".post")
     end
   
     def make_courses
